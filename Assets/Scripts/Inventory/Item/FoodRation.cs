e6f44d8f8e8cc8d004ddf4b00d7d7fbc9c1e0020
Zilow <zilow1234@gmail.com>
2019-04-23 07:18:59 +0100

    added food rations and changed prefabs

---
+++ Assets/Scripts/Inventory/Item/FoodRation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Inventory {
+namespace Items {
+public class FoodRation : Item {
+	public float hunger_heal = Common.hunger_heal;
+}
+
+}
+}
---
+++ Assets/Scripts/Inventory/ItemFactory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Inventory.Items;
+using UnityEngine;
+namespace Inventory {
+public class ItemFactory : MonoBehaviour {
+
+	public FirstAid first_aid_prefab;
+
+	public WaterBottle water_bottle_prefab;
+
+	public FoodRation food_rations;
+
+	public void create_first_aid(Vector3 position)
+	{
+		Debug.Log("Create first aid");
+		Instantiate(first_aid_prefab, position, new Quaternion(100.0f, 10.0f, 20.0f, -40.0f));
+	}
+
+	public void create_watter_bottle(Vector3 position)
+	{
+		Instantiate(water_bottle_prefab, position, new Quaternion(-10.0f, -20.0f, 10.0f, 10.0f));
+	}
+
+	public void create_ammo(Vector3 position, Weapon.EAmmoType ammotype, int amount)
+	{
+		Debug.Log("Item Factory ammotype = " + ammotype);
+		GameObject ammobox = Common.get_object_from_type(ammotype); 
+		GameObject obj = Instantiate(ammobox, position, new Quaternion(50.0f, 20.0f, 10.0f, 0.0f));
+		obj.GetComponent<AmmoBox>().ammo_amount = amount;
+	}
+
+	public void create_food_ration(Vector3 position)
+	{
+		Debug.Log("Create food rations");
+		Instantiate(food_rations, position, new Quaternion(-10.0f, -20.0f, 10.0f, 10.0f));
+	}
+}
+}// namespcae Inventory
